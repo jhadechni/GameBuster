cbee0d00eadfa123caec1c40b72a624859efcff0
jaime.sierra_pragma <jaime.sierra_pragma@JAIMESIERRA>
2022-10-15 10:14:47 -0500

    Agregar archivos de proyecto.

---
+++ ClientApp/src/setupProxy.js
@@ -0,0 +1,20 @@
+﻿const { createProxyMiddleware } = require('http-proxy-middleware')
+
+console.log(createProxyMiddleware)
+
+const target = process.env.ASPNETCORE_HTTPS_PORT ? `https://localhost:${process.env.ASPNETCORE_HTTPS_PORT}` :
+    process.env.ASPNETCORE_URLS ? process.env.ASPNETCORE_URLS.split(';')[0] : 'http://localhost:30490';
+
+// routes to redirect with the proxy
+const context = ["/weatherforecast"];
+
+module.exports = function (app) {
+    const appProxy = createProxyMiddleware(context, {
+        target: target,
+        secure: false,
+        headers: {
+            Connection: 'Keep-Alive'
+        }
+    });
+    app.use(appProxy);
+}
